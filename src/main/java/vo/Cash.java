5f21731d5eaa0aae47ac29413f4240cfca78d533
GDJ58 <GDJ58@DESKTOP-QM0A3Q1>
2022-11-21 10:11:21 +0900

    vo 생성(도메인 타입)

---
+++ src/main/java/vo/Cash.java
@@ -0,0 +1,48 @@
+package vo;
+
+public class Cash {
+	private int cashNo; //INNER JOIN -> Cash 타입
+	private String categoryNo;//FK INNER JOIN -> Map 타입
+	private long cashPrice;
+	private String cashMemo;
+	private String updatedate;
+	private String createdate;
+	public int getCashNo() {
+		return cashNo;
+	}
+	public void setCashNo(int cashNo) {
+		this.cashNo = cashNo;
+	}
+	public String getCategoryNo() {
+		return categoryNo;
+	}
+	public void setCategoryNo(String categoryNo) {
+		this.categoryNo = categoryNo;
+	}
+	public long getCashPrice() {
+		return cashPrice;
+	}
+	public void setCashPrice(long cashPrice) {
+		this.cashPrice = cashPrice;
+	}
+	public String getCashMemo() {
+		return cashMemo;
+	}
+	public void setCashMemo(String cashMemo) {
+		this.cashMemo = cashMemo;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+
+	
+}
